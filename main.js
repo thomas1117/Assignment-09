55951192d6ca7c0c8c7a6d3d1b35beefbaa29c62
Thomas Francis Shannon <Thomas@Thomas-MacBook-Pro.local>
2015-10-19 17:42:11 -0700

    added zero to hours

--- main.js
+++ main.js
@@ -9,6 +9,7 @@ var myClock=function(){
 var time = new Date();
 var hours = time.getHours();
 var newHours=(hours>12 ? hours - 12 : hours);
+newHours = (newHours<10 ? "0" : "") + newHours;
 var mins = time.getMinutes();
 var newMins=(mins<10 ? "0" : "")+mins;
 var secs  = time.getSeconds();
